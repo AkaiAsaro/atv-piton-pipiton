822de0be74e9c2675e43f3f52e862b071eb574c2
Luiz Augusto <162390772+AkaiAsaro@users.noreply.github.com>
2024-09-16 18:01:04 -0300

    Add files via upload

---
+++ aula 01/main.py
@@ -0,0 +1,18 @@
+# idade1 = input('digite uma idade: ')
+# idade2 = input('digite outra idade: ')
+
+# if idade1 > idade2:
+#     print(f' {idade1} é maior que {idade2}')
+# elif idade1 == idade2:
+#     print(f'{idade1} é igual a {idade2}')
+# elif idade1 < idade2:
+#     print(f'{idade1} é menor que {idade2}')
+
+
+
+# 13
+palavra1 = input("fala uma palavra aí: ")
+
+palavra2 = input("fala uma palavra aí: ")
+if palavra1 == palavra2:
+    print(f'{palavra1} é igual a {palavra2}')
